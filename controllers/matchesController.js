b8187e44f7fec3a7c01d1f714ebd7f5ac01ff10a
jaydeepw <jaydeep.w@gmail.com>
2019-07-01 17:47:14 +0530

    refactor: Remove methods used to verification of JavaScript semantics as I am not aware of using classes in JS way.

--- controllers/matchesController.js
+++ controllers/matchesController.js
@@ -1,13 +1,6 @@
 const fs = require('fs')
 
 class MatchesController {
-    
-    static getAllStudents(req, res) {
-          return res.status(200).json({
-                students,
-                message: "All the students",
-          });
-    }
 
     static sendResponseByFilter(dbPath, req, res) {
         fs.readFile(dbPath, 'utf8', function(err, contents) {
@@ -17,20 +10,6 @@ class MatchesController {
                 res.send(err)
             }
         });
-    }
-
-    // Get a single student
-    static getSingleStudent(req, res) {
-           const findStudent = students.find(student => student.id === parseInt(req.params.id, 10));
-           if (findStudent) {
-               return res.status(200).json({
-                     student: findStudent,
-                     message: "A single student record",
-               });
-           }
-           return res.status(404).json({
-                 message: "Student record not found",
-           });
     }
 }
 
