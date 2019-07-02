ee5a061061eecaf406c201a6e10c3d16bd63d85a
jaydeepw <jaydeep.w@gmail.com>
2019-07-02 15:33:53 +0530

    feature: Implement feture to filter matches by availability of a photo

--- controllers/matchesController.js
+++ controllers/matchesController.js
@@ -3,13 +3,33 @@ const fs = require('fs')
 class MatchesController {
 
     static sendResponseByFilter(dbPath, req, res) {
+        var matchesControllerScope = this
         fs.readFile(dbPath, 'utf8', function(err, contents) {
             if(!err) {
-                res.send(JSON.parse(contents))
+                contents = JSON.parse(contents)
+                console.log(contents.matches.length)
+                const result = matchesControllerScope.getFilteredData(req, contents)
+                console.log(result.matches.length)
+                res.send(result)
             } else {
                 res.send(err)
             }
         });
+    }
+
+    static getFilteredData(req, contents) {
+        const hasPhoto = req.query.hasPhoto
+        if(hasPhoto === 'true') {
+            contents.matches = contents.matches.filter(this.hasPhoto)
+        }
+            
+        return contents
+    }
+
+    static hasPhoto(value) {
+        return value.main_photo != null &&
+        value.main_photo != 'undefined' &&
+        value.main_photo != '';
     }
 }
 
